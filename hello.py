ceb4a2cb43227d508077ac38fddebccf114af295
Brittany Miller <brittanydmiller@gmail.com>
2014-08-12 15:39:50 -0700

    Adds basic templating and an active form

--- hello.py
+++ hello.py
@@ -1,8 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request, redirect, session
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template('index.html')
+
+@app.route('/goodbye')
+def goodbye():
+    return render_template('goodbye.html')
+
+@app.route('/numberform', methods=['POST'])
+def numberform():
+    session['number'] = request.form['number']
+    return redirect('/goodbye')
+
+if __name__ == '__main__':
+    app.secret_key = 'aasdifjapoaopsifpowj234234j0f9w8f209r8'
+    app.run(debug = True, use_reloader=True)
